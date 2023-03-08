45d09399daf4e5be4872f876a7f76c4ff2d7bef1
root <root@4ifs.l.time4vps.cloud>
2023-03-08 10:57:48 +0200

    lightdash first metrics

--- dbt/models/analytics/ads_summary.sql
+++ dbt/models/analytics/ads_summary.sql
@@ -25,8 +25,8 @@ summary as (
      , max(scrape_rank) over(partition by ad_id) as last_scrape_rank
 	 , count(*) over(partition by ad_id) as number_scraped
 	 , max(published_at) over(partition by ad_id) as published_at 
-from stg_ads_scrape_rank
-group by ad_id, url, model, trim, scrape_rank, price, seller, year, registered_on, km, transmission, engine, hp, color, efficiency, co2, site_rec, region, published_at, started_scrape_at
+	from stg_ads_scrape_rank
+	group by ad_id, url, model, trim, scrape_rank, price, seller, year, registered_on, km, transmission, engine, hp, color, efficiency, co2, site_rec, region, published_at, started_scrape_at
 ),
 
 set_status as (
